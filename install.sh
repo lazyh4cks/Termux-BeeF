2613f117c2b117ad56eb34bea6bcd164eafdb16a
ᑕYᗷEᖇKᑎIGᕼT <125413016+lazyh4cks@users.noreply.github.com>
2023-05-23 18:09:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,7 @@
 
 echo Installing dependences...
 sleep 1
-pkg install curl wget libyaml libxslt espeak ruby python nodejs autoconf automake bison clang cmake libffi libgmp libtool make patch pkg-config ruby2-dev
-git clone https://github.com/beefproject/beef
+pkg install curl wget libyaml libxslt espeak ruby python nodejs autoconf automake bison clang cmake libffi libgmp libtool make patch pkg-config
 echo Installing Gem...
 sleep 1
 gem install sqlite3 -- --with-cflags="-Wno-error=implicit-function-declaration"
@@ -14,6 +13,4 @@ gem install nokogiri -- --use-system-libraries
 mv script.sh ./beef/
 cd beef && rm -rf install
 bash script.sh
-
-
 
